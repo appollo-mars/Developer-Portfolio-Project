96c8314008a706b1dc975eba0ae9851c5e11fa51
evanssmaina <marsappollo3@gmail.com>
2023-07-10 20:28:37 +0300

    added responsive design

--- src/App.vue
+++ src/App.vue
@@ -4,55 +4,58 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   
-  <body class="bg-black text-white font_body">
-     <nav class="flex items-center justify-between py-7 px-25">
-      <h1 class="font-serif text-xl">Johnathan Specter</h1>
-        <ul class="flex items-center gap-x-8 text-base">
+  <body class="bg-black text-white">
+     <nav class="flex items-center justify-between lg:px-25 lg:pt-10 pl-10 pt-10">
+      <h1 class="font-serif lg:text-xl text-2xl ">Johnathan Specter</h1>
+      <div class="lg:block hidden">
+        <ul class="flex items-center gap-x-8 text-base  ">
           <li class="hover:text-yellow-300"><a href="#">Articles</a></li>
           <li class="hover:text-yellow-300"><a href="#">Chats</a></li>
           <li class="hover:text-yellow-300"><a href="#">Awards</a></li>
           <li class="hover:text-yellow-300"><a href="#">About</a></li>
          </ul>
-         <a href="#" class="text-black font-serif bg-yellow-300 hover:bg-yellow-500 px-7 py-4 text-lg">Get in touch</a>
+      </div>
+        
+         <a href="#" class="text-black font-serif bg-yellow-300 hover:bg-yellow-500 px-7 py-4 text-lg lg:block hidden">Get in touch</a>
      </nav>
       
-    <div class="py-20 px-25">
-      <div class="font-serif text-6xl">
-        <h1>Helping companies build<br> better, scalable software.</h1>
+    <div class="py-20 lg:px-25 px-10 lg:w-3/4 lg:text-left text-center">
+      <div class="font-serif lg:text-6xl text-5xl lg:mb-3">
+        <h1>Helping companies build better, scalable software.</h1>
       </div>
       <br>
 
       <div class="text-lg text-gray-400">
-        <p>Award-winning web developer and authour, with over 15+ years of working <br>experience with Fortune 500 companies like Apple, Facebook, and <br>more.</p>
+        <p>Award winning web developer and authour, with over 15+ years of working experience with Fortune 500 companies like Apple, Facebook, and more.</p>
       </div>
     </div>
 
-    <div class="flex items-center space-x-25 justify-center px-25">
+    <div class="flex lg:flex-row items-center lg:space-x-25 justify-center lg:px-25 space-x-5">
       <img src="./Assets/Logos/Walmart.svg">
       <img src="./Assets/Logos/JPMorgan.svg">
       <img src="./Assets/Logos/Visa.svg">
-      <img src="./Assets/Logos/Tinder.svg">
-      <img src="./Assets/Logos/Samsung.svg">
-      <img src="./Assets/Logos/Verizon.svg">
+      <img src="./Assets/Logos/Tinder.svg" class="lg:block hidden">
+      <img src="./Assets/Logos/Samsung.svg" class="lg:block hidden">
+      <img src="./Assets/Logos/Verizon.svg" class="lg:block hidden">
     </div>
 
-    <div class=" flex items-center justify-between px-25 py-20">
-      <div class="hero 1">
+    <div class=" flex lg:flex-row flex-col items-center justify-between lg:px-25 py-20 px-10">
+      <div class="hero 1 lg:mb-0 mb-12">
       <img src="./Assets/Spense.png" alt="">
       <br>
-      <h1 class="font-serif text-4xl">Spense.com →</h1>
+      <h1 class="font-serif text-4xl mb-3">Spense.com →</h1>
       <p class="text-lg text-gray-400">Rethinking the way writers get paid, an open-source platform<br>designed to help writers focus more on writing, and less on<br>when and how they'll get paid. Project in collaboration with<br>Codwell.cc</p>
       </div>
 
     <div class="hero2">
       <img src="./Assets/YelpCamp.png" alt="">
       <br>
-      <h1 class="font-serif text-4xl">YelpCamp.com →</h1>
+      <h1 class="font-serif text-4xl mb-3">YelpCamp.com →</h1>
       <p class="text-lg text-gray-400">Allowing backpack travelers to perfectly plan their trip <br>through an open-source platform similar to TripAdvisor. With <br> over 1m MAU , YelpCamp has been the crowd's favourite in <br>2021.</p>
     </div>
   </div>
     
-    <div class="flex items-center justify-between bg-gray-700 px-25 py-20">
+    <div class="flex lg:flex-row flex-col items-center justify-between bg-gray-700 lg:px-25 py-20 px-10">
       <div class="hero">
       <h1 class="font-serif text-4xl">A co-founder at one of the <br>world's largest communities.</h1>
       <br>
@@ -73,16 +76,15 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
     
 
-    <div class="px-25 py-20">
-      <h1 class="font-serif text-4xl">Interested in working with me?</h1>
-      <p class="text-lg text-gray-400">I'm active on all social media platforms listed below, but you <br>can also email and I will get to you within 24-48 <br>hours.</p>
-      <br>
+    <div class="lg:px-25 py-20 px-10">
+      <h1 class="font-serif text-4xl lg:mb-0 mb-3 lg:mb-3">Interested in working with me?</h1>
+      <p class="text-lg text-gray-400 lg:mb-10 mb-10">I'm active on all social media platforms listed below, but you can also email and I will get to you within 24-48 hours.</p>
       <a href="#" class="text-black font-serif bg-yellow-300 hover:bg-yellow-500 px-7 py-4 text-lg">Get in touch</a>
     </div>
  <footer>
-  <div class="flex items-center justify-between bg-gray-700 py-5">
-      <h2 class="font-serif px-25 text-xl text-white">Johnathan Specter</h2>
-      <div class="flex items-center space-x-3 px-20">
+  <div class="flex lg:flex-row flex-col lg:items-center lg:justify-between bg-gray-700 py-5 px-10 lg:px-0">
+      <h2 class="font-serif lg:px-25 text-xl text-white mb-3 lg:mb-0">Johnathan Specter</h2>
+      <div class="flex items-center space-x-3 lg:px-20">
       <a href=""><img src="./Assets/Icons/Twitter.svg" alt=""></a>
       <a href=""><img src="./Assets/Icons/LinkedIn.svg" alt=""></a>
       <a href=""><img src="./Assets/Icons/Github.svg" alt=""></a>
@@ -92,18 +94,3 @@ import HelloWorld from './components/HelloWorld.vue'
 
  </body>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
